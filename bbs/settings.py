346ee4375a219aa20b48571575b163974a59e6f4
邓旭东 <18571535121@163.com>
2019-03-29 14:58:14 +0800

    v1.06 添加django允许外部IP访问

--- bbs/settings.py
+++ bbs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$c$7tpddk7$ik%l66bf$^=bh0cpyl&8h57=dg817e@$t$q43as'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ["*",'dxd.com']
 
 
 # Application definition
